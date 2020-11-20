b56fe50619f182abf2f768026f2680080a743c1a
sumsung-ПК <zh.plastinin2015@gmail.com>
2020-11-20 09:54:31 +0300

    release lab1

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<?php
+class A{
+	
+}
+
+class B extends A{
+	public function __construct($a){
+		$this->a = $a;
+	}
+
+	protected $a;
+}
+
+class C extends B{
+	public function __construct($a, $b, $c){
+		$this->b = $b;
+        $this->c = $c;
+		parent::__construct($a);
+	}
+
+	protected $b;
+    protected $c;
+}
+
+$a1 = new A();
+$a2 = new A();
+$a3 = new A();
+$b4 = new B($a2);
+$c5 = new C($b4, $a1, $a3);
+var_dump($c5);
+
+?>
